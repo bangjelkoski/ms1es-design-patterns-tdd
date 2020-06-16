2bba087e2bf65c42e50bf062844c544ce536f2e8
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2020-06-16 21:35:31 +0200

    Abstract Factory tests #2

--- src/abstract-factory/index.spec.ts
+++ src/abstract-factory/index.spec.ts
@@ -1,4 +1,4 @@
-import { AbstractFactory } from './index';
+import { AbstractFactory, ConcreteFactory } from './index';
 
 describe('Abstract Factory Implementation using TDD', () => {
   test('there should be an AbstractFactory interface', () => {
@@ -15,5 +15,25 @@ describe('Abstract Factory Implementation using TDD', () => {
     };
 
     expect(isAbstractFactory(object)).toBe(true);
+  });
+
+  test('there should be an ConcreteFactory that implements the AbstractFactory', () => {});
+
+  test('there ConcreteFactory should have a method that creates an returns concrete product', () => {
+    const concreteFactory = new ConcreteFactory();
+
+    const isConcreteFactory = (object: any): object is ConcreteFactory => {
+      return 'createProduct' in object;
+    };
+
+    const object = {
+      createProduct(): any {},
+    };
+
+    expect(isConcreteFactory(object)).toBe(true);
+
+    const concreteProduct = concreteFactory.createProduct();
+
+    expect(concreteProduct).toBeDefined();
   });
 });
