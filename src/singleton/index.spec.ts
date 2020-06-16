35a6a17d7a54cd9dffcbed46b1cae29745a4eda3
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2020-06-16 22:33:33 +0200

    Singleton tests #1

---
+++ src/singleton/index.spec.ts
@@ -0,0 +1,30 @@
+describe('Abstract Factory Implementation using TDD', () => {
+  test('there should be a Singleton class', () => {
+    //
+  });
+
+  test('there should be a private constructor in the Singleton class, to prevent making instances with the construction calls with the `new` operator', () => {
+    // Javascript doesnt offer this functionality, so returning this as true
+    expect(true).toBe(true);
+  });
+
+  test('the Singleton class should have a static private instance property', () => {
+    // Javascript doesnt offer this functionality, so returning this as true
+    expect(true).toBe(true);
+  });
+
+  test('the Singleton class should have a getInstance method that returns an instance of the Singleton Class', () => {
+    const singleton = Singleton.getInstance();
+
+    expect(singleton).toBeInstanceOf(Singleton);
+  });
+
+  test('the Singleton class should have a getInstance method that returns ALWAYS the same instance of the Singleton Class', () => {
+    const singleton1 = Singleton.getInstance();
+    const singleton2 = Singleton.getInstance();
+
+    expect(singleton1).toBeInstanceOf(Singleton);
+    expect(singleton2).toBeInstanceOf(Singleton);
+    expect(singleton1).toEqual(singleton2);
+  });
+});
