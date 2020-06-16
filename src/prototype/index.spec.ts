5793a35325a263cc5589bdb80b9e46669ba8a22a
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2020-06-16 23:07:42 +0200

    Prototype implementation #1

--- src/prototype/index.spec.ts
+++ src/prototype/index.spec.ts
@@ -1,23 +1,62 @@
+import { Prototype } from './index';
+
 describe('Prototype Implementation using TDD', () => {
   test('there should be an Prototype interface', () => {
     //
   });
-  test('the Prototype interface should have a clone method that returns an instance of the Prototype interface', () => {
-    //
+  test('the Prototype interface should have a clone method', () => {
+    const isPrototype = (object: any): object is Prototype => {
+      return 'clone' in object;
+    };
+
+    const object = {
+      clone(): any {},
+    };
+
+    expect(isPrototype(object)).toBe(true);
   });
   test('the Prototype interface should have a simple property that will be cloned', () => {
+    const isPrototype = (object: any): object is Prototype => {
+      return 'id' in object;
+    };
+
+    const object = {
+      id: '',
+    };
+
+    expect(isPrototype(object)).toBe(true);
+  });
+  test('there should be an Concrete Prototype class that implements the Prototype interface', () => {
     //
   });
-  test('there should be an ConcretePrototype class that implements the Prototype interface', () => {
-    //
+  test('the Concrete Prototype should have a clone method', () => {
+    const concretePrototype = new ConcretePrototype();
+
+    const isConcretePrototype = (object: any): object is ConcretePrototype => {
+      return 'clone' in object;
+    };
+
+    const object = {
+      clone(): any {},
+    };
+
+    expect(isConcretePrototype(object)).toBe(true);
+
+    const concretePrototypeClone = concretePrototype.clone();
+
+    expect(concretePrototypeClone).toBeDefined();
   });
-  test('the Concrete Prototype should have a clone method that returns a clone of the Concrete Prototype', () => {
-    //
-  });
-  test('the Concrete Prototype should have a simple property that will be cloned', () => {
-    //
+  test('the Prototype interface clone method should return an instance of the Prototype interface', () => {
+    const concretePrototype = new ConcretePrototype();
+    const concretePrototypeClone = concretePrototype.clone();
+
+    expect(concretePrototypeClone).toBeInstanceOf(ConcretePrototype);
   });
   test('after cloning, cloned object as well as the simple property should be the same between the original and cloned object', () => {
-    //
+    const concretePrototype = new ConcretePrototype();
+    const concretePrototypeClone = concretePrototype.clone();
+
+    expect(concretePrototype).toEqual(concretePrototypeClone);
+    expect(concretePrototype.id).toEqual(concretePrototypeClone.id);
   });
 });
--- src/prototype/index.ts
+++ src/prototype/index.ts
@@ -2,3 +2,7 @@
  * Prototype is a creational design pattern that allows cloning objects,
  * even complex ones, without coupling to their specific classes.
  */
+export interface Prototype {
+  id: string;
+  clone(): Prototype;
+}
