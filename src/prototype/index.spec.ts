045a78dfe8879a7aae448eebe0c86fa20f385712
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2020-06-16 22:57:41 +0200

    Prototype tests

---
+++ src/prototype/index.spec.ts
@@ -0,0 +1,23 @@
+describe('Prototype Implementation using TDD', () => {
+  test('there should be an Prototype interface', () => {
+    //
+  });
+  test('the Prototype interface should have a clone method that returns an instance of the Prototype interface', () => {
+    //
+  });
+  test('the Prototype interface should have a simple property that will be cloned', () => {
+    //
+  });
+  test('there should be an ConcretePrototype class that implements the Prototype interface', () => {
+    //
+  });
+  test('the Concrete Prototype should have a clone method that returns a clone of the Concrete Prototype', () => {
+    //
+  });
+  test('the Concrete Prototype should have a simple property that will be cloned', () => {
+    //
+  });
+  test('after cloning, cloned object as well as the simple property should be the same between the original and cloned object', () => {
+    //
+  });
+});
---
+++ src/prototype/index.ts
@@ -0,0 +1,4 @@
+/**
+ * Prototype is a creational design pattern that allows cloning objects,
+ * even complex ones, without coupling to their specific classes.
+ */
