50619477ec32248eca433f93f14970f24e28bf78
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2020-06-16 21:39:07 +0200

    Abstract Factory tests #3

--- src/abstract-factory/index.spec.ts
+++ src/abstract-factory/index.spec.ts
@@ -1,4 +1,4 @@
-import { AbstractFactory, ConcreteFactory } from './index';
+import { AbstractFactory, ConcreteFactory, AbstractProduct } from './index';
 
 describe('Abstract Factory Implementation using TDD', () => {
   test('there should be an AbstractFactory interface', () => {
@@ -17,9 +17,9 @@ describe('Abstract Factory Implementation using TDD', () => {
     expect(isAbstractFactory(object)).toBe(true);
   });
 
-  test('there should be an ConcreteFactory that implements the AbstractFactory', () => {});
+  test('there should be an ConcreteFactory class that implements the AbstractFactory', () => {});
 
-  test('there ConcreteFactory should have a method that creates an returns concrete product', () => {
+  test('the ConcreteFactory should have a method that creates an returns concrete product', () => {
     const concreteFactory = new ConcreteFactory();
 
     const isConcreteFactory = (object: any): object is ConcreteFactory => {
@@ -35,5 +35,19 @@ describe('Abstract Factory Implementation using TDD', () => {
     const concreteProduct = concreteFactory.createProduct();
 
     expect(concreteProduct).toBeDefined();
+  });
+
+  test('there should be a abstract ConcreteProduct interface', () => {});
+
+  test('the ConcreteProduct interface should contain at least one method', () => {
+    const isAbstractProduct = (object: any): object is AbstractProduct => {
+      return 'usefulMethod' in object;
+    };
+
+    const object = {
+      usefulMethod(): any {},
+    };
+
+    expect(isAbstractProduct(object)).toBe(true);
   });
 });
